4599d35d5443fa35247a63576ad71206b7ae5eb2
Luciano Palma <lucianopalma90@gmail.com>
2017-07-10 11:49:01 +0100

    remove redundant else clause

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -25,26 +25,26 @@ public class TennisGame1 implements TennisGame {
 
         if (anyPlayerScoredFour()) {
             return formatAdvantageOrWinScore();
-        } else {
-            for (int i=1; i<3; i++)
+        }
+        
+        for (int i=1; i<3; i++)
+        {
+            if (i==1) tempScore = m_score1;
+            else { score+="-"; tempScore = m_score2;}
+            switch(tempScore)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                case 0:
+                    score+="Love";
+                    break;
+                case 1:
+                    score+="Fifteen";
+                    break;
+                case 2:
+                    score+="Thirty";
+                    break;
+                case 3:
+                    score+="Forty";
+                    break;
             }
         }
         return score;
