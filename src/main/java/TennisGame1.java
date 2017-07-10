af54189f43593e4f3ea79553e013b3a07c00b2b9
Luciano Palma <lucianopalma90@gmail.com>
2017-07-10 11:48:46 +0100

    free second if-statement from previous else-clause

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -23,12 +23,8 @@ public class TennisGame1 implements TennisGame {
             return formatDrawScore();
         }
 
-        if (m_score1>=4 || m_score2>=4) {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        if (anyPlayerScoredFour()) {
+            return formatAdvantageOrWinScore();
         } else {
             for (int i=1; i<3; i++)
             {
@@ -52,6 +48,20 @@ public class TennisGame1 implements TennisGame {
             }
         }
         return score;
+    }
+
+    private String formatAdvantageOrWinScore() {
+        String score;
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private boolean anyPlayerScoredFour() {
+        return m_score1>=4 || m_score2>=4;
     }
 
     private String formatDrawScore() {
